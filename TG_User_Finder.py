1868201d3702374abc447efc3748eaf64644f422
ffffff333334 <fifociccio81@gmail.com>
2024-07-12 21:28:38 +0200

    TG_User_Finder.py

--- TG_User_Finder.py
+++ TG_User_Finder.py
@@ -37,8 +37,8 @@ def list_checker():
             print ("Null")
 
 if __name__ == '__main__':
-    phone = 'Your TG Number'
-    client = TelegramClient(phone, API ID, 'API Hash')
+    phone = '+39 347 953 3738'
+    client = TelegramClient(phone,'27627790','b355e2da6a92e098d0fe0ecc36dc9fc2')
     client.connect()
     if not client.is_user_authorized():
         client.send_code_request(phone)
